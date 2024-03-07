745485bbe88783e6b4bbb930d5693d2a86523de6
Lehar1107 <126607616+Lehar1107@users.noreply.github.com>
2024-03-07 23:04:19 +0530

    Update jenelk.sh

--- jenelk.sh
+++ jenelk.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# Function to check if Docker is installed
+dockerinstall() {
+    if ! command -v docker &> /dev/null; then
+        echo "Docker is not installed"
+        exit 1
+    fi
+}
 
 dockerinstall
 
@@ -6,9 +15,7 @@ case $choice in
         elasticsearch)
            component="elasticsearch"
         container_name="elasticsearch-container-$(date +%Y%m%d%H%M%S)"
-        /usr/bin/docker run -d --name "$container_name" -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.0
-
-
+        docker run -d --name "$container_name" -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.0
         echo "Elasticsearch is now running in a Docker container."
                 ;;
         kibana) echo "you select kibana"
